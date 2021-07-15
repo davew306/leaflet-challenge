cf8b38066b6a3f5ff8933c6497d49bc359c22edf
David Wu <davew306@gmail.com>
2021-07-15 14:08:27 -0400

    update

---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZGF2ZXczMDYiLCJhIjoiY2txbGhncTgxMHE3dzJwbXZhMmRlcWxrMCJ9.RJeFvCqRZI6yPGtNf4R8Qw";
---
+++ static/js/logic.js
@@ -0,0 +1,103 @@
+
+var background = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=API_KEY);
+
+var map = L.map("mapid", {
+  center: [37.09, -95.71],
+  zoom: 5,
+  layers: [background]
+});
+
+graymap_background.addTo(map);
+
+var base = {
+  Satellite: background,
+};
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", function(data) {
+
+
+  function style(feature) {
+    return {
+      opacity: .9,
+      fillOpacity: .9,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.4
+    };
+  }
+
+  function CaseColor(magnitude) {
+    switch (true) {
+      case magnitude > 5:
+        return "#0066cc";
+      case magnitude > 4:
+        return "#0080ff";
+      case magnitude > 3:
+        return "#4da6ff";
+      case magnitude > 2:
+        return "#80bfff";
+      case magnitude > 1:
+        return "#b3d9ff";
+      default:
+        return "#e6f2ff";
+    }
+  }
+
+
+  function radius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+
+    return magnitude * 3;
+  }
+
+
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+      return L.circleMarker(latlng);
+    },
+    style: styleInfo,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+  }).addTo(earthquakes);
+
+  earthquakes.addTo(map);
+
+
+  var legend = L.control({
+    position: "bottomright"
+  });
+
+
+  legend.onAdd = function() {
+    var div = L
+      .DomUtil
+      .create("div", "info legend");
+
+    var grades = [0, 1, 2, 3, 4, 5];
+    var colors = [
+      "#e6f2ff",
+      "#b3d9ff",
+      "#80bfff",
+      "#4da6ff",
+      "#0080ff",
+      "#0066cc"
+    ];
+
+
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+        grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+    }
+    return div;
+  };
+
+
+  legend.addTo(map);
+
+});
